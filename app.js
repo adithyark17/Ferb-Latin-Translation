bd6ac0f9fd80279ad62b0b0b406279cc0ac78a9d
Adithya Rajesh <52730950+adithyark17@users.noreply.github.com>
2021-08-05 17:30:36 +0530

    updated code

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+var btn = document.querySelector("#btn")
+var txt = document.querySelector("txt-input")
+var output = document.querySelector("#output")
+
 var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+function getTranslationURL (text) {
+    return serverURL + "?"+ "text=" + text;
+}
+
+function errorHandler(error){
+    console.log (" Error occured", error);
+    alert("Something wrong with server, Try again later");
+
+}
+
+
+
+function clickEventHandler(){
+    var input = txt.value;
+
+    fetch(getTranslationURL(input))
+        .then (response => response.json)
+        .then (json => {
+            outputDiv.innerText = json.contents.translated;
+        })
+        .catch(errorHandler)
+}
+
+btn.addEventListener("click",clickEventHandler)
